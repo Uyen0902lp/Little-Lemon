617bd5f9b6c56c3a71e9b3ff4115893e8ae1ec33
Uyen0902lp <uyenlp0902@gmail.com>
2024-04-17 23:30:26 +0700

    update hero-section and reserve

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
 import Reserve from './components/Reserve/Reserve'
+import HeroSection from './components/HeroSection/HeroSection'
+
+
 
 function App() {
   return (
     <>
     <NavBar/>
+    <HeroSection/>
     <Reserve/>
     </>
   );
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './hero.css'
+import image from '../../assets/images/plate2.png'
 // 20:56-38:00
 const HeroSection = () => {
   return (
-    <div>navbar</div>
+    <div className="hero-section">
+      <div className="hero-left">
+        <div className="left-intro">
+          <p>Enjoy the Mediterranean cuisine with reasonable price</p>
+        </div>
+        <div className="left-welcome">
+          <p>Welcome to Little Lemon</p>
+        </div>
+        <div className="left-des">
+          <p>Experience the essence of Mediterranean charm at Little Lemon. Savor a curated menu that blends tradition with a modern twist, all in a cozy atmosphere that feels like home. Little Lemon — where every meal is a celebration of culture and taste.</p>
+        </div>
+        <div className="left-btn">
+          <button>Explore Menu</button>
+        </div>
+      </div>
+      <div className="hero-right">
+        <div className="right-img">
+          <img src={image} alt="Right plate" />
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
