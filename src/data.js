c7139e8ebc358bca978827d2ba7e5086ae282ed0
Uyen0902lp <uyenlp0902@gmail.com>
2024-04-18 00:00:30 +0700

    update data

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const dishes = [
+    {
+        id: 1,  
+        name: 'Bucatini',
+        image: require('./assets/images/bucatini.gif'), 
+        description: 'Twirled Bucatini, peppery greens, shaved Parmesan zest.',
+        price: '$10.99'
+    },
+    {
+        id: 2,  
+        name: 'Capricciosa',
+        image: require('./assets/images/plate.png'), 
+        description: 'This is the description for Dish 2',
+        price: '$7.50'
+    },
+    {
+        id: 3,  
+        name: 'Casunziei',
+        image: require('./assets/images/casunziei1.png'), 
+        description: 'This is the description for Dish 3',
+        price: '$12.45'
+    },
+    {
+        id: 4,  
+        name: 'Diablo',
+        image: require('./assets/images/diablo.png'), 
+        description: 'This is the description for Dish 4',
+        price: '$9.20'
+    },
+    {
+        id: 5,  
+        name: 'Pepperoni',
+        image: require('./assets/images/periperi.png'), 
+        description: 'This is the description for Dish 5',
+        price: '$9.99'
+    },
+    {
+        id: 6,  
+        name: 'Saporita',
+        image: require('./assets/images/saporita.png'), 
+        description: 'This is the description for Dish 6',
+        price: '$10.60'
+    },
+    {
+        id: 7,  
+        name: 'Farfalle',
+        image: require('./assets/images/saporita (1).png'), 
+        description: 'This is the description for Dish 7',
+        price: '$15.00'
+    },
+    {
+        id: 8,  
+        name: 'Fettucine',
+        image: require('./assets/images/fettucine.png'), 
+        description: 'This is the description for Dish 8',
+        price: '$10.20'
+    },
+    {
+        id: 9,  
+        name: 'Burger',
+        image: require('./assets/images/burger.29a4a17e79dd6a3d4b36.png'), 
+        description: 'This is the description for Dish 9',
+        price: '$14.55'
+    },
+  ];
+  
